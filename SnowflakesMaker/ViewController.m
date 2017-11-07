31bb6c58ab7f7e4a546f832e62dd8f3baa913d87
Oleksiy Radyvanyuk <oleksiy@ngti.nl>
2017-11-07 10:15:18 +0100

    Correct snow flake for the screen scale.

--- SnowflakesMaker/ViewController.m
+++ SnowflakesMaker/ViewController.m
@@ -84,9 +84,10 @@ NSInteger const kMaxPhases = 3;
 
 - (UIImage *)createSnowflake {
     // creating image for the quarter size of the image view
+    CGFloat scale = UIScreen.mainScreen.scale;
     CGSize size = self.imgView.frame.size;
-    size.width = roundf(size.width / 4);
-    size.height = roundf(size.height / 4);
+    size.width = scale * roundf(size.width / 4);
+    size.height = scale * roundf(size.height / 4);
     CGRect rect = CGRectMake(0.0f, 0.0f, floorf(size.width / 2), floorf(size.height / 2));
 
     CGImageRef branchRef = [self randomizeOneSnowflakeBranchInRect:rect];
